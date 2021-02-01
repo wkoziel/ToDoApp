7b0207a9523302c5bfc74055a44d48af55ca6038
Wojciech Kozieł <44378819+fern00@users.noreply.github.com>
2021-02-01 12:40:28 +0100

    Update wylog.php
    Naciśnięcie wyloguje, poza wylogowaniem ustawia nas spowrotem na stronie logowania

--- wylog.php
+++ wylog.php
@@ -15,16 +15,18 @@ tworz_naglowek_html('Wylogowanie');
 if (!empty($stary_uzyt)) {
   if ($wynik_niszcz) {
     // jeżeli użytkownik zalogowany i nie wylogowany
-    echo 'Wylogowano.<br />';
-    tworz_HTML_URL('logowanie.php', 'Logowanie');
+    // echo 'Wylogowano.<br />';
+    // tworz_HTML_URL('logowanie.php', 'Logowanie');
+    header("Location: http://localhost/ToDoApp/logowanie.php");
+    die();
   } else {
    // użytkownik zalogowany i wylogowanie niemożliwe
     echo 'Wylogowanie niemożliwe.<br />';
   }
 } else {
   // jeżeli brak zalogowania, lecz w jakiś sposób uzyskany dostęp do strony
-  echo 'Użytkownik niezalogowany, tak więc brak wylogowania.<br />';
-  tworz_HTML_URL('logowanie.php', 'Logowanie');
+  header("Location: http://localhost/ToDoApp/logowanie.php");
+  die();
 }
 
 tworz_stopke_html();
