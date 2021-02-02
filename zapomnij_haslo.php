27d525ae5c67555b65756299e397892d21f0e14b
Tatiana Cieślar <tatianacieslar23@gmail.com>
2021-02-02 13:16:15 +0100

    Widok zapomnij_haslo.php

--- zapomnij_haslo.php
+++ zapomnij_haslo.php
@@ -8,11 +8,32 @@
   try {
      $haslo=ustaw_haslo($nazwa_uz);
      powiadom_haslo($nazwa_uz, $haslo);
-     echo 'Nowe hasło zostało przesłane na adres poczty elektronicznej.<br />';
+      echo "<br>";
+      echo '<h1>Nowe hasło zostało przesłane na adres poczty elektronicznej.</h1>';
+      echo "<br>";
+      echo "<div class=\"mini-stopka\">";
+      echo "<div class=\"stopka-link\">";
+      echo "<a href=\"logowanie.php\"><h2>Logowanie</h2></a>";  
+      echo "</div>";
+      echo "<div class=\"stopka-link\">";
+      echo "<a href=\"formularz_rejestracji.php\"><h2>Rejestracja</h2></a>";  
+      echo "</div>";
+      echo "</div>";
   }
   catch (Exception $e) {
-     echo 'Hasło nie mogło zostać ustawione. Proszę spróbować później.';
+      echo '';
+      echo "<br>";
+      echo '<h1>Hasło nie mogło zostać ustawione. Proszę spróbować później.</h1>';
+      echo "<br>";
+      echo "<div class=\"mini-stopka\">";
+      echo "<div class=\"stopka-link\">";
+      echo "<a href=\"logowanie.php\"><h2>Logowanie</h2></a>";  
+      echo "</div>";
+      echo "<div class=\"stopka-link\">";
+      echo "<a href=\"formularz_rejestracji.php\"><h2>Rejestracja</h2></a>";  
+      echo "</div>";
+      echo "</div>";
   }
-  tworz_HTML_URL('logowanie.php', 'Logowanie');
+
   tworz_stopke_html();
 ?>
